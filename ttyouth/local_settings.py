aa77834eccdd7430db3934b2c91e4003afc48b29
Dave <dave@daverobinson.me>
2020-08-18 13:35:47 -0400

    style

--- ttyouth/local_settings.py
+++ ttyouth/local_settings.py
@@ -4,6 +4,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['www.ttyouth.org']
 STATIC_ROOT = []
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
